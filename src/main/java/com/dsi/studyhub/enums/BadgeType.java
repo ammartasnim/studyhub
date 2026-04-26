458801011d1513da716dd9b0e56cf267be874ef3
khalil <ckhalil308@gmail.com>
2026-04-26 13:48:30 +0100

    dto fixed and other changesd

---
+++ src/main/java/com/dsi/studyhub/enums/BadgeType.java
@@ -0,0 +1,14 @@
+package com.dsi.studyhub.enums;
+
+public enum BadgeType {
+    BEGINNER,
+    LEARNER,
+    EXPLORER,
+    CONTRIBUTOR,
+    HELPER,
+    CONSISTENT,
+    COLLABORATOR,
+    ACHIEVER,
+    MENTOR,
+    LEGEND
+}
