410b40792a20d6b520564125c92571b50d856fc3
Tasnim Ammar <tasnimammar2005@gmail.com>
2026-05-01 17:50:16 +0100

    level tweak

--- src/main/java/com/dsi/studyhub/gamification/GamificationService.java
+++ src/main/java/com/dsi/studyhub/gamification/GamificationService.java
@@ -36,10 +36,13 @@ public class GamificationService {
         boolean alreadyEarned = user.getBadges().stream()
                 .anyMatch(b -> b.getType() == newBadgeType);
 
-        int newLevel = newBadgeType.ordinal() + 1;
-        if (newLevel > user.getLevel()) {
-            user.setLevel(newLevel);
-        }
+//        int newLevel = newBadgeType.ordinal() + 1;
+        int newLevel= calculateLevel(newXp);
+        user.setLevel(newLevel);
+
+        // Debugging logs - Check these in your terminal!
+        System.out.println("DEBUG: User " + user.getUsername() + " now has " + newXp + " XP");
+        System.out.println("DEBUG: Calculated Level: " + newLevel);
 
         if (!alreadyEarned) {
             Badge badgeEntity = new Badge();
@@ -67,16 +70,16 @@ public class GamificationService {
         return BadgeType.BEGINNER;
     }
 
-//    private int calculateLevel(int totalXp) {
-//        if (totalXp >= 35000) return 10; // Legend
-//        if (totalXp >= 20000) return 9;  // Mentor
-//        if (totalXp >= 12000) return 8;  // Achiever
-//        if (totalXp >= 7000)  return 7;  // Collaborator
-//        if (totalXp >= 4000)  return 6;  // Consistent
-//        if (totalXp >= 2000)  return 5;  // Helper
-//        if (totalXp >= 1000)  return 4;  // Contributor
-//        if (totalXp >= 500)   return 3;  // Explorer
-//        if (totalXp >= 200)   return 2;  // Learner
-//        return 1; // Beginner
-//    }
+    private int calculateLevel(int totalXp) {
+        if (totalXp >= 35000) return 10; // Legend
+        if (totalXp >= 20000) return 9;  // Mentor
+        if (totalXp >= 12000) return 8;  // Achiever
+        if (totalXp >= 7000)  return 7;  // Collaborator
+        if (totalXp >= 4000)  return 6;  // Consistent
+        if (totalXp >= 2000)  return 5;  // Helper
+        if (totalXp >= 1000)  return 4;  // Contributor
+        if (totalXp >= 500)   return 3;  // Explorer
+        if (totalXp >= 200)   return 2;  // Learner
+        return 1; // Beginner
+    }
 }
